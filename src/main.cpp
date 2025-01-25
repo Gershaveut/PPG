7868730cec819da5581620dabbf91d89ab885ac4
Gershaveut <vova1234567890012011@yandex.ru>
2025-01-25 19:07:28 +0300

    add score

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,58 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Graphics/Rect.hpp>
 #include <SFML/Graphics/RectangleShape.hpp>
+#include <SFML/Graphics/Text.hpp>
 #include <SFML/Graphics/Transformable.hpp>
 #include <SFML/System/Vector2.hpp>
 #include <SFML/Window/Keyboard.hpp>
+#include <string>
+
+unsigned int width = 1920;
+unsigned int height = 1080;
+
+sf::Vector2f center({width / 2.f, height / 2.f});
+
+float playerSpeed = 1.50;
+float playerBotSlowing = 1.50;
+
+unsigned int playerScore;
+unsigned int playerBotScore;
+
+sf::Vector2f playerShape({15, 50});
+
+sf::RectangleShape player(playerShape);
+sf::RectangleShape playerBot(playerShape);
+
+sf::CircleShape ball(10.f);
+
+sf::Vector2f ballDirection;
+
+sf::Font font("arial.ttf");
+sf::Text score(font, "0 : 0");
+
+void clearScreen() {
+  ball.setPosition({center.x, center.y});
+
+  player.setPosition({25, center.y});
+  playerBot.setPosition({width - 25.f, center.y});
+
+  ballDirection = {1, -1};
+}
+
+void updateScore() {
+  clearScreen();
+
+  score.setString(std::to_string(playerScore) + " : " +
+                  std::to_string(playerBotScore));
+}
 
 int main() {
-  unsigned int width = 400;
-  unsigned int height = 200;
-
-  float playerSpeed = 1.50;
-
   auto window = sf::RenderWindow(sf::VideoMode({width, height}), "PPG");
   window.setFramerateLimit(120);
 
-  sf::Vector2f ballDirection;
-  ballDirection = {1, -1};
+  clearScreen();
 
-  sf::CircleShape ball(25.f);
-
-  float ballScale = ball.getScale().x;
-
-  ball.setOrigin({ballScale / 2, ballScale / 2});
-  ball.setPosition({width / 2.f, width / 2.f});
-
-  sf::Vector2f playerShape({15, 50});
-
-  sf::RectangleShape player(playerShape);
-  sf::RectangleShape playerBot(playerShape);
-  player.setPosition({25, height / 2.f});
-  playerBot.setPosition({width - 25.f, height / 2.f});
-
-  sf::RectangleShape screen({width - ballScale, height - ballScale});
+  score.setPosition({0, center.y});
 
   while (window.isOpen()) {
     while (const std::optional event = window.pollEvent()) {
@@ -39,6 +60,8 @@ int main() {
         window.close();
       }
     }
+
+    sf::Vector2f ballPosition = ball.getPosition();
 
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Down)) {
       player.move({0, 1 * playerSpeed});
@@ -48,10 +71,10 @@ int main() {
       player.move({0, -1 * playerSpeed});
     }
 
-    if (playerBot.getPosition().y < ball.getPosition().y) {
-      playerBot.move({0, 1 * playerSpeed});
+    if (playerBot.getPosition().y < ballPosition.y) {
+      playerBot.move({0, 1 * playerSpeed / playerBotSlowing});
     } else {
-      playerBot.move({0, -1 * playerSpeed});
+      playerBot.move({0, -1 * playerSpeed / playerBotSlowing});
     }
 
     auto ballBounds = ball.getGlobalBounds();
@@ -61,16 +84,16 @@ int main() {
       ballDirection.x *= -1;
     }
 
-    if (!ballBounds.findIntersection(screen.getGlobalBounds())) {
-      if (ball.getPosition().x > screen.getSize().x) {
-        ballDirection.x = -1;
-      } else if (ball.getPosition().x < 0) {
-        ballDirection.x = 1;
-      } else if (ball.getPosition().y > screen.getSize().y) {
-        ballDirection.y = -1;
-      } else {
-        ballDirection.y = 1;
-      }
+    if (ballPosition.x > width) {
+      ++playerScore;
+      updateScore();
+    } else if (ballPosition.x < 0) {
+      ++playerBotScore;
+      updateScore();
+    } else if (ballPosition.y > height) {
+      ballDirection.y = -1;
+    } else if (ballPosition.y < 0) {
+      ballDirection.y = 1;
     }
 
     ball.move(ballDirection);
@@ -79,6 +102,7 @@ int main() {
     window.draw(ball);
     window.draw(player);
     window.draw(playerBot);
+    window.draw(score);
     window.display();
   }
 }
